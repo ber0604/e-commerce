8ec0b58794cf5a708a53bae737447f4eafca9021
Bernardo Marquetti <bernardo.060404@bento.local>
2022-06-28 17:24:26 -0300

    First commit with laravel

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,47 @@
+@include('templates.head')
+<header class="container-fluid p-3 bg-black text-danger">
+    <div class="header conatiner">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="home.php" class=" col-sm nav-link px-2 text-danger ">Home</a></li>
+          <li><a href="streaming.php" class="col-sm nav-link px-2 text-danger ">Streaming</a></li>
+          <li><a href="conta.php" class="col-sm nav-link px-2 text-danger  ">Minha conta</a></li>
+          <li>
+            <label for="genero">Gênero:</label>
+            <select id="generos" name="generos" onchange="location = this.value;">
+              <option value=""></option>
+              <option value="acao.php">Ação</option>
+              <option value="infantil.php">Infantil</option>
+              <option value="romance.php">Romance</option>
+
+            </select>
+          </li>
+        </ul>
+
+        <div class="busca">
+          <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input type="search" class="form-control form-control-dark" placeholder="Buscar..." aria-label="Search">
+          </form>
+        </div>
+
+        <div class="login">
+          <a href="login.php" class="col-sm nav-link px-2 text-danger ">Login</a>
+        </div>
+
+        <div class="cart-btn">
+          <i id="cart" class="fas fa-shopping-cart"></i>
+          <span class="cart-quantity">0</span>
+        </div>
+
+
+      </div>
+      <div class="membro">
+        <a href="membro.php" class=" col-sm nav-link px-2 text-danger ">SEJA MEMBRO</a>
+      </div>
+  </header>
+
+@yield('content')
+
+
+@include('templates.footer')
